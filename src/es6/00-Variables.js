829508c55390fc606eff7c83682013add72db4b9
José Poleo <jose.poleo.2a@gmail.com>
2023-09-14 11:21:22 -0400

    FEAT:Aprendiendo Arroww function y variales

---
+++ src/es6/00-Variables.js
@@ -0,0 +1,23 @@
+var nombre = 'Jose';
+console.log(nombre);
+
+let fruit = 'Apple';
+console.log(fruit);
+
+const animal = 'dog';
+console.log(animal);
+
+const fruit = ()=>{
+    if(true){
+        var fruit1 = 'Apple';
+        let fruit2 = 'Jiwi';
+        const fruit3 = 'Banana';
+    }
+
+    console.log(fruit1)
+    console.log(fruit2)
+    console.log(fruit3);
+
+}
+
+fruit();
---
+++ src/es6/01-arrow.js
@@ -0,0 +1,14 @@
+function square(num){
+
+    return num * num;
+}
+
+const square = (num) => {
+    return num * num;
+}
+
+console.log(square(4));
+
+const square = (num) => num * num;
+
+console.log(square(5))
